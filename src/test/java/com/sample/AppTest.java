b3a6eb2c26ea1b604b847c3ccc7d7f54e516bf87
Felipe Kunzler <felipe.kunzler@sap.com>
2018-03-17 19:48:32 -0300

    Create project

---
+++ src/test/java/com/sample/AppTest.java
@@ -0,0 +1,18 @@
+package com.sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+
+    @Test
+    public void testSum() {
+        App app = new App();
+        assertEquals(app.sum(1, 1), 2);
+        assertEquals(app.sum(0, 0), 0);
+        assertEquals(app.sum(-1, -2), -3);
+    }
+
+}
